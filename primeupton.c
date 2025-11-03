757b01117d3b87cd8572efc01a5ca0885cabd775
zobiya526 <zobiyashaik38@gmail.com>
2025-11-03 14:21:09 +0530

    Add files via upload

---
+++ primeupton.c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+    int n,i,count,j;
+    printf("enter number ");
+    scanf("%d",&n);
+    for(j=2;j<=n;j++){
+        
+    count=0;
+    for(i=2;i<=sqrt(j);i++){
+        if(j%i==0){
+            count++;
+            break;
+        }
+    }
+        if(count==0)
+        printf("%d ",j);
+        
+    }
+    
+
+    return 0;
+}
